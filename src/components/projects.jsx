ea4822beba5f6f3679df1b19d03a313d51033dca
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-10 15:35:03 -0500

    Had to remove some projects I was unable to deploy on time :/

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -58,57 +58,10 @@ export default class Projects extends Component {
 									</div>
 								</div>
 							</div>
-							{/*<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Drum Kit</a></h3>
-											<span>Application</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>*
-												<span><a href="https://inpen.github.io/drumKit/" target="_blank" rel="noopener noreferrer"><i className="icon-eye" /></a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Coffee Shop Rating App</a></h3>
-											<span>Graphic, Logo</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="https://inpen.github.io/Tetris/" target="_blank" rel="noopener noreferrer"><i className="icon-eye" /></a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Reloj No Marques Las Horas~</a></h3>
-											<span>Web Design</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="https://inpen.github.io/relojNoMarquesLasHoras/" target="_blank" rel="noopener noreferrer"><i className="icon-eye" /></a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
+
+
+
 						</div>
-						<div className="row">
-							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
-							</div>
-						</div>*/}
 					</div>
         </section>
       </div>
